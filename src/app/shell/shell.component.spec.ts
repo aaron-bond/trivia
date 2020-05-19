f92a6ec06e084cada11aabab90ad76ee61df7837
aaron-bond <aaron-bond@users.noreply.github.com>
2020-05-19 07:57:04 +0100

    Update shell.component.spec.ts

--- src/app/shell/shell.component.spec.ts
+++ src/app/shell/shell.component.spec.ts
@@ -2,9 +2,8 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 import { ShellComponent } from './shell.component';
-import { OpenTriviaAPI } from 'services/open-trivia.api';
+import { OpenTriviaAPI, SocketService, GameService } from 'services';
 import { Category } from 'model';
-import { SocketService } from '../../services/socket.service';
 
 class OpenTriviaAPISpy {
     public getQuestions: jasmine.Spy = jasmine.createSpy('getQuestions').and.callFake((category: Category) => {});
@@ -16,6 +15,10 @@ class SocketServiceSpy {
     public messageReceived: jasmine.Spy = jasmine.createSpyObj('messageReceived', ['subscribe']);
 }
 
+class GameServiceSpy {
+    public LobbySynchronised: jasmine.Spy = jasmine.createSpyObj('LobbySynchronised', ['subscribe']);
+}
+
 describe('ShellComponent', () => {
     let component: ShellComponent;
     let fixture: ComponentFixture<ShellComponent>;
@@ -25,7 +28,9 @@ describe('ShellComponent', () => {
             declarations: [ShellComponent],
             providers: [
                 { provide: OpenTriviaAPI, useClass: OpenTriviaAPISpy },
-                { provide: SocketService, useClass: SocketServiceSpy }
+                { provide: SocketService, useClass: SocketServiceSpy },
+
+                { provide: GameService, useClass: GameServiceSpy }
             ],
             schemas: [CUSTOM_ELEMENTS_SCHEMA]
         }).compileComponents();
